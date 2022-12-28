944d6a2f1b5d6e1d1f54e0ba3de3464fd2117b49
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-29 03:13:10 +0700

    [PATCH] minor change

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -12,10 +12,6 @@ function M.config()
 			diagnostics = "nvim_lsp",
 		},
 	})
-
-	vim.keymap.set("n", "<C-h>", ":bp<CR>")
-	vim.keymap.set("n", "<C-l>", ":bn<CR>")
-	vim.keymap.set("n", "<C-d>", ":BD<CR>")
 end
 
 return M
