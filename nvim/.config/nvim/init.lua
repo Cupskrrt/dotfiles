f99ea643e289c222901c31e3c347f0fe0631faa2
Luthfi Khan <cupskrrtt@Cupskrrtt.local>
2023-05-08 19:02:51 +0700

    [PATCH] Emergency update to nvim package

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,8 @@
-require("base")
-require("highlight")
-require("keymaps")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
-end
-vim.opt.runtimepath:prepend(lazypath)
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("config.base")
+require("config.highlight")
+require("config.keymaps")
 
 local has = function(x)
 	return vim.fn.has(x) == 1
@@ -23,52 +12,9 @@ local is_mac = has("macunix")
 local is_win = has("win32")
 
 if is_mac then
-	require("macos")
+	require("config.macos")
 end
 
 if is_win then
-	require("windows")
-end
-
-require("lazy").setup("plugins", {
-	defaults = { lazy = true },
-	install = { colorscheme = { "kanagawa" } },
-	checker = { enabled = true },
-	change_detection = {
-		notify = false,
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"matchit",
-				"matchparen",
-				"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
-})
-
-local builtins = {
-	"fzf",
-	"getscript",
-	"getscriptPlugin",
-	"vimball",
-	"vimballPlugin",
-	"2html_plugin",
-	"matchit",
-	"matchparen",
-	"logiPat",
-	"rrhelper",
-	"netrw",
-	"netrwPlugin",
-	"netrwSettings",
-	"netrwFileHandlers",
-}
-
-for _, plugin in ipairs(builtins) do
-	vim.g["loaded_" .. plugin] = 1
+	require("config.windows")
 end
